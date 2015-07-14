b3e7f00a08dbfb4f97fcdea6cbaa09780fc1802b
John Van Tuyl <jpvantuyl@gmail.com>
2015-07-14 15:38:52 -0700

    about triangle project 2 done

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,12 +18,19 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    set_of_sides = set([a, b, c])
-    if len(set_of_sides) == 1:
-      return 'equilateral'
-    elif len(set_of_sides) == 2:
-      return 'isosceles'
-    return 'scalene'
+  sides = sorted([a,b,c])
+  
+  for side in sides:
+    if side < 1: raise TriangleError
+  if sides[0] + sides[1] < sides[2]: raise TriangleError
+  
+  
+  set_of_sides = set(sides)
+  if len(set_of_sides) == 1:
+    return 'equilateral'
+  elif len(set_of_sides) == 2:
+    return 'isosceles'
+  return 'scalene'
 
 
 # Error class used in part 2.  No need to change this code.
